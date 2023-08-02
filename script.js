f4ca2b40f3c02558eab7527c169890f65b481d10
tahawebdev <139816232+tahawebdev@users.noreply.github.com>
2023-08-02 19:58:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+    var element = document.body;
+    var navElement = document.getElementById("navElement");
+    navElement.classList.toggle("light-mode")
+    element.classList.toggle("dark-mode");
+ }
+
+ 
